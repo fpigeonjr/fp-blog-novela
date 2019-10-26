5e6ed392727252bcb7421e68b16b4931f4a916f6
Frank Pigeon <frank.pigeonjr@gmail.com>
2019-10-26 00:45:13 -0500

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,56 @@
+module.exports = {
+  siteMetadata: {
+    title: `Blog by fpigeonjr`,
+    name: `Frank Pigeon`,
+    siteUrl: `https://frankpigeon.com`,
+    description: `This is my description that will be used in the meta tags and important for search results`,
+    hero: {
+      heading: `code | linux | music | life`,
+      maxWidth: 652
+    },
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/fpigeonjr`
+      },
+      {
+        name: `github`,
+        url: `https://github.com/fpigeonjr`
+      },
+      {
+        name: `instagram`,
+        url: `https://instagram.com/fpigeonjr`
+      },
+      {
+        name: `linkedin`,
+        url: `https://www.linkedin.com/company/narative/`
+      }
+    ]
+  },
+  plugins: [
+    {
+      resolve: '@narative/gatsby-theme-novela',
+      options: {
+        contentPosts: 'content/posts',
+        contentAuthors: 'content/authors',
+        basePath: '/',
+        authorsPage: true,
+        sources: {
+          local: true
+        }
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Novela by Narative`,
+        short_name: `Novela`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `standalone`,
+        icon: `src/assets/favicon.png`
+      }
+    }
+  ]
+}
